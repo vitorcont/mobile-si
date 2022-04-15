3fff369496a9cd3ce42d037fce5b470831e7fa80
vitor-conti <vitor.conti@mblabs.com.br>
2022-04-15 19:59:44 -0300

    ci(eslint): configured eslint rules an added prettier

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@mobile': './src',
+          },
+          extensions: [
+            '.native.ts',
+            '.native.tsx',
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+        },
+      ],
+    ],
   };
 };
