34463c4cc93695e56c54c39b4a1c879eb5b241af
Renato Santos <404renato@gmail.com>
2022-04-28 09:48:22 -0300

    feat: stylization

--- App.tsx
+++ App.tsx
@@ -4,8 +4,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import FlashMessage from 'react-native-flash-message';
 import { Provider } from 'react-redux';
+import { Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 const App = () => {
+  let [fontsloaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+  });
+
+  if (!fontsloaded) return <AppLoading />;
+
   return (
     <Provider store={store}>
       <StatusBar style="auto" />
@@ -13,6 +23,6 @@ const App = () => {
       <AppContent />
     </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    primary: '#A200FF',
+  },
+  fonts: {
+    Regular: 'Poppins_400Regular',
+    Bold: 'Poppins_700Bold',
+  },
+};
